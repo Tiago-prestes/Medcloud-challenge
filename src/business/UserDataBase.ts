22ff7a126fcb614cb242939bb53475f6fb9cc072
TIAGO PRESTES <83032703+Tiago-prestes@users.noreply.github.com>
2021-10-20 18:21:07 -0300

    Get e Post adicionados

--- src/business/UserDataBase.ts
+++ src/business/UserDataBase.ts
@@ -5,10 +5,16 @@ export class UserDataBase extends BaseDatabase {
 
     private static TABLE_NAME = "medcloud_users"
 
-    async create(user: User) {
+    async createUser(user: User) {
         await BaseDatabase
             .connection(UserDataBase.TABLE_NAME)
             .insert(user)
     }
+
+    getAllUser = async () => {
+        const result = await BaseDatabase
+            .connection(UserDataBase.TABLE_NAME)
+            return result
+      }
 
 }
