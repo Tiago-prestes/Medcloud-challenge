a74cd30fe02058caacbb9f86c284a5d11debe069
TIAGO PRESTES <83032703+Tiago-prestes@users.noreply.github.com>
2021-10-26 15:01:05 -0300

    reorganizando

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,6 @@
 import { app } from './app'
 import { createUser } from './endpoint/creatUser'
-import{ getAllUser } from './endpoint/getAllUsers'
+import { getAllUser } from './endpoint/getAllUsers'
 
 app.get('/listusers', getAllUser)
 app.post('/users', createUser)
